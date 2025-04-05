f73e0f0d3598ac0d447654099f3b63a8d708b5cb
Filipe Pina <636320+fopina@users.noreply.github.com>
2025-04-05 16:27:07 +0100

    feature: classyclick.argument (#8)

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,13 +1,69 @@
 from unittest import TestCase
 
-from classyclick import command
+from click.testing import CliRunner
+
+import classyclick
 
 
 class Test(TestCase):
+    @property
+    def click_version(self):
+        from click import __version__
+
+        return tuple(map(int, __version__.split('.')))
+
     def test_error(self):
         def not_a_class():
-            @command()
+            @classyclick.command()
             def hello():
                 pass
 
         self.assertRaisesRegex(ValueError, 'hello is not a class', not_a_class)
+
+    def test_option(self):
+        @classyclick.command()
+        class Hello:
+            name: str = classyclick.option(help='Name')
+
+            def __call__(self):
+                print(f'Hello, {self.name}')
+
+        runner = CliRunner()
+        result = runner.invoke(Hello, ['--name', 'Peter'])
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, 'Hello, Peter\n')
+
+    def test_argument(self):
+        @classyclick.command()
+        class Hello:
+            name: str = classyclick.argument()
+
+            def __call__(self):
+                print(f'Hello, {self.name}')
+
+        runner = CliRunner()
+        result = runner.invoke(Hello)
+        self.assertEqual(result.exit_code, 2)
+
+        if self.click_version >= (8, 0):
+            self.assertIn("Error: Missing argument 'NAME'", result.output)
+        elif self.click_version >= (7, 0):
+            self.assertIn('Error: Missing argument "NAME"', result.output)
+        else:
+            self.assertIn('Error: Missing argument "name"', result.output)
+
+        result = runner.invoke(Hello, ['--help'])
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(
+            result.output,
+            """\
+Usage: hello [OPTIONS] NAME
+
+Options:
+  --help  Show this message and exit.
+""",
+        )
+
+        result = runner.invoke(Hello, ['Peter'])
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, 'Hello, Peter\n')
