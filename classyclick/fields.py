651b111e4ef621c301e75622b5e0c84156b38bf5
Filipe Pina <hzlu1ot0@duck.com>
2025-05-12 14:54:08 +0100

    x

--- classyclick/fields.py
+++ classyclick/fields.py
@@ -38,25 +38,25 @@ def argument(*, type=None, **attrs: Any) -> 'ClassyArgument':
     return ClassyArgument(attrs=attrs)
 
 
-def context() -> 'ClassyContextObj':
+def context() -> 'ClassyContext':
     """
     ...
     """
-    return ClassyContextObj(None)
+    return ClassyContext(attrs=None)
 
 
 def context_obj() -> 'ClassyContextObj':
     """
     ...
     """
-    return ClassyContextObj(None)
+    return ClassyContextObj(attrs=None)
 
 
-def context_meta() -> 'ClassyContextObj':
+def context_meta(key: str, **attrs: Any) -> 'ClassyContextObj':
     """
     ...
     """
-    return ClassyContextObj(None)
+    return ClassyContextMeta(key=key, attrs=attrs)
 
 
 @dataclass(frozen=True)
@@ -79,6 +79,7 @@ class ClassyField:
 
     def __call__(self, command: 'Command', field: 'Field'):
         """To be implemented in subclasses"""
+        return command
 
 
 @dataclass(frozen=True)
@@ -86,7 +87,7 @@ class ClassyArgument(ClassyField):
     def __call__(self, command: 'Command', field: 'Field'):
         self.infer_type(field)
 
-        self.click.argument(field.name, **self.attrs)(command)
+        return self.click.argument(field.name, **self.attrs)(command)
 
 
 @dataclass(frozen=True)
@@ -117,16 +118,32 @@ class ClassyOption(ClassyField):
             del self.attrs['type']
             self.attrs['is_flag'] = True
 
-        self.click.option(*param_decls, **self.attrs)(command)
+        return self.click.option(*param_decls, **self.attrs)(command)
 
 
 @dataclass(frozen=True)
 class ClassyContext(ClassyField):
+    def store_field_name(self, command: 'Command', field: 'Field'):
+        if not hasattr(command, '__classy_context__'):
+            command.__classy_context__ = []  # type: ignore
+        command.__classy_context__.insert(0, field.name)
+
     def __call__(self, command: 'Command', field: 'Field'):
-        pass
+        self.store_field_name(command, field)
+        return self.click.pass_context(command)
 
 
 @dataclass(frozen=True)
-class ClassyContextObj(ClassyField):
+class ClassyContextObj(ClassyContext):
     def __call__(self, command: 'Command', field: 'Field'):
-        pass
+        self.store_field_name(command, field)
+        return self.click.pass_obj(command)
+
+
+@dataclass(frozen=True)
+class ClassyContextMeta(ClassyContext):
+    key: str
+
+    def __call__(self, command: 'Command', field: 'Field'):
+        self.store_field_name(command, field)
+        return self.click.decorators.pass_meta_key(self.key, **self.attrs)(command)
--- tests/test_command_context.py
+++ tests/test_command_context.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 
 import click
 from click.testing import CliRunner
@@ -8,7 +9,7 @@ from tests import BaseCase
 
 
 class Test(BaseCase):
-    def test_argument(self):
+    def test_context(self):
         # example from https://click.palletsprojects.com/en/stable/complex/#the-root-command
         class Repo(object):
             def __init__(self, home=None, debug=False):
@@ -31,8 +32,49 @@ class Test(BaseCase):
             def __call__(self):
                 click.echo(f'Clone from {self.src} to {self.dest} at {self.repo.home}')
 
+        @classyclick.command(group=cli)
+        class Clone2:
+            ctx: Any = classyclick.context()
+            src: str = classyclick.argument()
+            dest: str = classyclick.argument(required=False)
+
+            def __call__(self):
+                click.echo(f'Clone from {self.src} to {self.dest} at {self.ctx.obj.home}')
+
         runner = CliRunner()
-        result = runner.invoke(cli, args=['clone', '1'])
+
+        result = runner.invoke(cli, args=['clone', '1', '2'])
         self.assertIsNone(result.exception)
         self.assertEqual(result.exit_code, 0)
-        self.assertRegex(result.output, 'Clone from 1 to None at .*?/\.repo\n')
+        self.assertRegex(result.output, r'Clone from 1 to 2 at .*?/\.repo\n')
+
+        result = runner.invoke(cli, args=['clone2', '1', '2'])
+        self.assertIsNone(result.exception)
+        self.assertEqual(result.exit_code, 0)
+        self.assertRegex(result.output, r'Clone from 1 to 2 at .*?/\.repo\n')
+
+    def test_context_meta(self):
+        @click.group()
+        @click.option('--repo-home', envvar='REPO_HOME', default='.repo')
+        @click.option('--debug/--no-debug', default=False, envvar='REPO_DEBUG')
+        @click.pass_context
+        def cli(ctx, repo_home, debug):
+            ctx.meta['repo'] = repo_home
+            ctx.meta['debug'] = debug
+
+        @classyclick.command(group=cli)
+        class Clone:
+            repo: str = classyclick.context_meta('repo')
+            debug: bool = classyclick.context_meta('debug')
+            src: str = classyclick.argument()
+            dest: str = classyclick.argument(required=False)
+
+            def __call__(self):
+                click.echo(f'Clone from {self.src} to {self.dest} at {self.repo}')
+
+        runner = CliRunner()
+
+        result = runner.invoke(cli, args=['clone', '1', '2'])
+        self.assertEqual(result.output, 'Clone from 1 to 2 at .repo\n')
+        self.assertIsNone(result.exception)
+        self.assertEqual(result.exit_code, 0)
