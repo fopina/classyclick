808fce26171be2e9372fc6520b6a7c553f003bf8
Filipe Pina <hzlu1ot0@duck.com>
2026-03-09 10:04:05 +0000

    x

--- tests/clis/test_test_hello_readme.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-
-def test_test_hello_readme_file_exists():
-    assert (Path(__file__).resolve().parents[1] / 'test_hello_readme.py').is_file()
--- tests/clis/test_test_hello_readme2.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-
-def test_test_hello_readme2_file_exists():
-    assert (Path(__file__).resolve().parents[1] / 'test_hello_readme2.py').is_file()
