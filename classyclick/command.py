117213d5a4f5b58a0a7945f998743eeeaaabcdb8
Filipe Pina <hzlu1ot0@duck.com>
2026-03-13 15:59:39 +0000

    x

--- classyclick/command.py
+++ classyclick/command.py
@@ -1,15 +1,11 @@
-from dataclasses import dataclass, fields
+from dataclasses import fields
 from typing import Callable, Protocol, TypeVar, Union
-
-try:
-    from typing import dataclass_transform
-except ImportError:
-    from typing_extensions import dataclass_transform
 
 import click
 
 from . import utils
 from .fields import Argument, Context, ContextMeta, ContextObj, Option, _Field
+from .group import _build_click_class_command
 
 T = TypeVar('T')
 
@@ -46,7 +42,7 @@ def command(cls=None, *, group=None, **click_kwargs) -> Callable[[T], Union[T, C
         func.__name__ = utils.camel_snake(kls.__name__)
 
         # at the end so it doesn't affect __doc__ or others
-        _strictly_typed_dataclass(kls)
+        utils.strictly_typed_dataclass(kls)
 
         # apply options
         # apply in reverse order to match click's behavior - it DOES MATTER when multiple click.argument
@@ -66,62 +62,11 @@ def command(cls=None, *, group=None, **click_kwargs) -> Callable[[T], Union[T, C
     return _wrapper(cls)
 
 
-def _strictly_typed_dataclass(kls):
-    annotations = getattr(kls, '__annotations__', {})
-    for name, val in kls.__dict__.items():
-        if name.startswith('__'):
-            continue
-        if name not in annotations and isinstance(val, _Field):
-            raise TypeError(f"{kls.__module__}.{kls.__qualname__} is missing type for classy field '{name}'")
-    return dataclass(kls)
-
-
-def _build_click_class_command(cls, *, is_group=False):
-    _strictly_typed_dataclass(cls)
-
-    def func(*args, **kwargs):
-        if args:
-            args = list(args)
-            ctx = getattr(func, '__classy_context__', [])
-            for field_name in ctx:
-                kwargs[field_name] = args.pop()
-        cls(*args, **kwargs)()
-
-    func.__doc__ = cls.__doc__
-    func.__name__ = utils.camel_snake(cls.__name__)
-
-    for field in fields(cls)[::-1]:
-        if isinstance(field, _Field):
-            func = field(func)
-
-    click_kwargs = {}
-    config = getattr(cls, '__config__', None)
-    if isinstance(config, Command.Config):
-        click_kwargs.update(vars(config))
-
-    for name, value in cls.__dict__.items():
-        if name.startswith('__click_') and name.endswith('__'):
-            key = name[len('__click_') : -2]
-            if key and key not in click_kwargs:
-                click_kwargs[key] = value
-
-    click_group = click_kwargs.pop('group', None)
-    if click_group is None:
-        click_group = click
-    if is_group:
-        cls.__command__ = click_group.group(**click_kwargs)(func)
-    else:
-        if issubclass(click_group, Group):
-            click_group = click_group.click
-        cls.__command__ = click_group.command(**click_kwargs)(func)
-    cls.click = cls.__command__
-
-
-@dataclass_transform(field_specifiers=(Option, Argument, Context, ContextObj, ContextMeta))
+@utils.dataclass_transform(field_specifiers=(Option, Argument, Context, ContextObj, ContextMeta))
 class Command:
     """Base class for class-based click commands."""
 
-    __config__ = None
+    __config__: 'Config | None' = None
     """
     Customize the command with click arguments setting this to a classyclick.Command.Config instance
     """
@@ -141,6 +86,3 @@ class Command:
     @classmethod
     def _build_click_command(cls):
         _build_click_class_command(cls, is_group=False)
-
-
-from .group import Group
--- classyclick/group.py
+++ classyclick/group.py
@@ -1,10 +1,60 @@
-from .command import _build_click_class_command, dataclass_transform
-from .fields import Argument, Context, ContextMeta, ContextObj, Option
+from dataclasses import fields
+
+import click
+
+from . import utils
+from .fields import Argument, Context, ContextMeta, ContextObj, Option, _Field
 
 
-@dataclass_transform(field_specifiers=(Option, Argument, Context, ContextObj, ContextMeta))
+def _build_click_class_command(cls, *, is_group=False):
+    utils.strictly_typed_dataclass(cls)
+
+    def func(*args, **kwargs):
+        if args:
+            args = list(args)
+            ctx = getattr(func, '__classy_context__', [])
+            for field_name in ctx:
+                kwargs[field_name] = args.pop()
+        cls(*args, **kwargs)()
+
+    func.__doc__ = cls.__doc__
+    func.__name__ = utils.camel_snake(cls.__name__)
+
+    for field in fields(cls)[::-1]:
+        if isinstance(field, _Field):
+            func = field(func)
+
+    click_kwargs = {}
+    config = getattr(cls, '__config__', None)
+    if getattr(config, '__dict__', None):
+        click_kwargs.update(vars(config))
+
+    for name, value in cls.__dict__.items():
+        if name.startswith('__click_') and name.endswith('__'):
+            key = name[len('__click_') : -2]
+            if key and key not in click_kwargs:
+                click_kwargs[key] = value
+
+    click_group = click_kwargs.pop('group', None)
+    if click_group is None:
+        click_group = click
+    if is_group:
+        cls.__command__ = click_group.group(**click_kwargs)(func)
+    else:
+        if isinstance(click_group, type) and issubclass(click_group, Group):
+            click_group = click_group.click
+        cls.__command__ = click_group.command(**click_kwargs)(func)
+    cls.click = cls.__command__
+
+
+@utils.dataclass_transform(field_specifiers=(Option, Argument, Context, ContextObj, ContextMeta))
 class Group:
     """Base class for class-based click groups."""
+
+    __config__: 'Config | None' = None
+    """
+    Customize the group with click arguments setting this to a classyclick.Group.Config instance
+    """
 
     class Config:
         def __init__(self, *, name: str = None, help: str = None, **kwargs):
