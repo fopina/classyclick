f5b577032cad89f7a2ad296b00d10ce8238d3118
Filipe Pina <hzlu1ot0@duck.com>
2026-03-17 01:42:40 +0000

    x

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-from .readme_tester import ReadmeParsingTestCase
+from readme_example_tester import ReadmeTestCase
 
 
-class TestReadme(ReadmeParsingTestCase):
+class TestReadme(ReadmeTestCase):
     README_PATH = Path(__file__).resolve().parents[1] / 'README.md'
     TESTS_DIR = Path(__file__).resolve().parent
