45efc6ff943c8b7db0a919df7866727571f4682b
Filipe Pina <hzlu1ot0@duck.com>
2026-03-28 08:02:54 +0000

    x

---
+++ classyclick/helpers/autodiscover.py
@@ -0,0 +1,10 @@
+import importlib
+import pkgutil
+
+
+def discover_commands(commands_package: str):
+    """Import every command module, including ones nested in subpackages."""
+    package = importlib.import_module(commands_package)
+    # str() required because of py3.10
+    for _, module_name, _ in pkgutil.walk_packages([str(package.__package__)], f'{commands_package}.'):
+        importlib.import_module(module_name)
